7ea709421891b977690ce76fb60d97762e5c52fd
Aly ezz <alyezz94@gmail.com>
2019-09-06 20:57:10 +0200

    ci

--- sampleApp/platforms/android/app/src/androidTest/java/com/instabug/cordova/plugin/InvokeInstabugUITest.java
+++ sampleApp/platforms/android/app/src/androidTest/java/com/instabug/cordova/plugin/InvokeInstabugUITest.java
@@ -27,49 +27,13 @@ public class InvokeInstabugUITest {
 
     @Test
     public void ensureInstabugInvocati1on() throws InterruptedException {
-        disableScreenShotByMediaProjection();
         Thread.sleep(1000);
         onView(withResourceName("instabug_floating_button")).perform(click());
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         onView(withText("Report a bug")).perform(click());
-        Thread.sleep(1000);
         onView(withResourceName("instabug_edit_text_email")).perform(replaceText("inst@bug.com"));
         onView(withResourceName("instabug_bugreporting_send")).perform(click());
         onView(withResourceName("instabug_success_dialog_container")).perform(click());
     }
 
-    private void disableScreenShotByMediaProjection() {
-        try {
-            Method method = getMethod(Class.forName("com.instabug.bug.BugReporting"), "setScreenshotByMediaProjectionEnabled", boolean.class);
-            if (method != null) {
-                method.invoke(null, false);
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Method getMethod(Class clazz, String methodName, Class... parameterType) {
-        final Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            if (method.getName().equals(methodName) && method.getParameterTypes().length ==
-                    parameterType.length) {
-                for (int i = 0; i < parameterType.length; i++) {
-                    if (method.getParameterTypes()[i] == parameterType[i]) {
-                        if (i == method.getParameterTypes().length - 1) {
-                            method.setAccessible(true);
-                            return method;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-        return null;
-    }
 }
