7311001f164f81cd648c7f54a41a5bf8fab95ccd
Aly ezz <alyezz94@gmail.com>
2019-09-07 02:09:13 +0200

    ci

--- sampleApp/platforms/android/app/src/androidTest/java/com/instabug/cordova/plugin/InvokeInstabugUITest.java
+++ sampleApp/platforms/android/app/src/androidTest/java/com/instabug/cordova/plugin/InvokeInstabugUITest.java
@@ -1,20 +1,29 @@
 package com.instabug.cordova.plugin;
 
+import android.support.test.espresso.PerformException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.util.HumanReadables;
+import android.support.test.espresso.util.TreeIterables;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.concurrent.TimeoutException;
 
 import io.cordova.hellocordova.MainActivity;
+import io.cordova.hellocordova.R;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -28,13 +37,61 @@ public class InvokeInstabugUITest {
     @Test
     public void ensureInstabugInvocati1on() throws InterruptedException {
         Thread.sleep(5000);
+        onView(isRoot()).perform(waitId(R.id.instabug_floating_button, 5000));
         onView(withResourceName("instabug_floating_button")).perform(click());
-        Thread.sleep(2000);
+        onView(isRoot()).perform(waitId(2131296560, 5000));
         onView(withText("Report a bug")).perform(click());
-        Thread.sleep(2000);
+        onView(isRoot()).perform(waitId(2131296517, 5000));
         onView(withResourceName("instabug_edit_text_email")).perform(replaceText("inst@bug.com"));
         onView(withResourceName("instabug_bugreporting_send")).perform(click());
-        onView(withResourceName("instabug_success_dialog_container")).perform(click());
+        onView(isRoot()).perform(waitId(2131296563, 5000));
     }
+
+    /**
+     * Perform action of waiting for a specific view id.
+     * @param viewId The id of the view to wait for.
+     * @param millis The timeout of until when to wait for.
+     */
+    public static ViewAction waitId(final int viewId, final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "wait for a specific view with id <" + viewId + "> during " + millis + " millis.";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                uiController.loopMainThreadUntilIdle();
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis;
+                final Matcher<View> viewMatcher = withId(viewId);
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                // timeout happens
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
+            }
+        };
+    }
+
 
 }
