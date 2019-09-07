a9dec6ad85405a683d3bbb0161e3697be67b6729
Aly ezz <alyezz94@gmail.com>
2019-09-07 03:30:43 +0200

    ci

--- sampleApp/platforms/android/app/src/androidTest/java/com/instabug/cordova/plugin/InvokeInstabugUITest.java
+++ sampleApp/platforms/android/app/src/androidTest/java/com/instabug/cordova/plugin/InvokeInstabugUITest.java
@@ -47,7 +47,7 @@ public class InvokeInstabugUITest {
 
     @Test
     public void ensureInstabugInvocati1on() throws InterruptedException {
-        Thread.sleep(10000);
+        Thread.sleep(20000);
         Instabug.setWelcomeMessageState(WelcomeMessage.State.DISABLED);
         //onView(withResourceName("instabug_floating_button")).perform(click());
         Instabug.show();
