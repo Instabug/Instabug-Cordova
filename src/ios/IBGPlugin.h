8d5abff51d9924ae2ad746aa7019ac6e1238de34
Ali Abdelfattah <ali.abdelfattah.xa@gmail.com>
2020-10-29 02:52:30 +0200

    Add iOS API setUserAttribute

--- src/ios/IBGPlugin.h
+++ src/ios/IBGPlugin.h
@@ -1,39 +1,41 @@
 #import <Cordova/CDVPlugin.h>
 
-@interface IBGPlugin : CDVPlugin 
+@interface IBGPlugin : CDVPlugin
 
-- (void) activate:(CDVInvokedUrlCommand*)command;
+- (void)activate:(CDVInvokedUrlCommand *)command;
 
-- (void) setPrimaryColor:(CDVInvokedUrlCommand*)command;
+- (void)setPrimaryColor:(CDVInvokedUrlCommand *)command;
 
-- (void) setUserData:(CDVInvokedUrlCommand*)command;
+- (void)setUserData:(CDVInvokedUrlCommand *)command;
 
-- (void) addFile:(CDVInvokedUrlCommand*)command;
+- (void)addFile:(CDVInvokedUrlCommand *)command;
 
-- (void) addLog:(CDVInvokedUrlCommand*)command;
+- (void)addLog:(CDVInvokedUrlCommand *)command;
 
-- (void) changeInvocationEvent:(CDVInvokedUrlCommand*)command;
+- (void)changeInvocationEvent:(CDVInvokedUrlCommand *)command;
 
-- (void) setLocale:(CDVInvokedUrlCommand*)command;
+- (void)setLocale:(CDVInvokedUrlCommand *)command;
 
-- (void) setReportTypes:(CDVInvokedUrlCommand*)command;
+- (void)setReportTypes:(CDVInvokedUrlCommand *)command;
 
-- (void) show:(CDVInvokedUrlCommand*)command;
+- (void)show:(CDVInvokedUrlCommand *)command;
 
-- (void) showBugReportingWithReportTypeAndOptions:(CDVInvokedUrlCommand*)command;
+- (void)showBugReportingWithReportTypeAndOptions:(CDVInvokedUrlCommand *)command;
 
-- (void) setBugReportingEnabled:(CDVInvokedUrlCommand*)command;
+- (void)setBugReportingEnabled:(CDVInvokedUrlCommand *)command;
 
-- (void) setChatsEnabled:(CDVInvokedUrlCommand*)command;
+- (void)setChatsEnabled:(CDVInvokedUrlCommand *)command;
 
-- (void) setRepliesEnabled:(CDVInvokedUrlCommand*)command;
+- (void)setRepliesEnabled:(CDVInvokedUrlCommand *)command;
 
-- (void) showChats:(CDVInvokedUrlCommand*)command;
+- (void)showChats:(CDVInvokedUrlCommand *)command;
 
-- (void) hasChats:(CDVInvokedUrlCommand*)command;
+- (void)hasChats:(CDVInvokedUrlCommand *)command;
 
-- (void) showReplies:(CDVInvokedUrlCommand*)command;
+- (void)showReplies:(CDVInvokedUrlCommand *)command;
 
-- (void) identifyUserWithEmail:(CDVInvokedUrlCommand*)command;
+- (void)identifyUserWithEmail:(CDVInvokedUrlCommand *)command;
+
+- (void)setUserAttribute:(CDVInvokedUrlCommand *)command;
 
 @end
