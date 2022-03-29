ea7611b3506ca9b95da3b6bd572ce90fe4ade08c
Youssef Raafat <yraafat@instabug.com>
2022-03-29 18:38:04 +0200

    Integrate Instabug plugin in example

--- example/www/js/index.js
+++ example/www/js/index.js
@@ -1,29 +1,14 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+  document.getElementById("deviceready").classList.add("ready");
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+  // Initialize Instabug SDK
+  cordova.plugins.instabug.start(
+    "cb518c145decef204b0735cb7efa6016",
+    [cordova.plugins.bugReporting.invocationEvents.button],
+    () => console.log("Instabug initialized."),
+    (error) => console.log("Instabug could not be initialized - " + error)
+  );
 }
