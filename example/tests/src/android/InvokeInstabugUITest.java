4d4e2a2a5c91d15595063cb35cba2ddb29083309
Youssef Raafat <yraafat@instabug.com>
2022-04-05 13:36:01 +0200

    Add Android tests

---
+++ example/tests/src/android/InvokeInstabugUITest.java
@@ -0,0 +1,34 @@
+package com.instabug.example;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class InvokeInstabugUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureInstabugInvocation() throws InterruptedException {
+        Thread.sleep(5000);
+        onView(withResourceName("instabug_floating_button")).perform(click());
+
+        onView(withText("Report a bug")).perform(click());
+        onView(withResourceName("instabug_edit_text_email")).perform(replaceText("inst@bug.com"));
+        onView(withResourceName("instabug_bugreporting_send")).perform(click());
+        onView(withResourceName("instabug_success_dialog_container")).perform(click());
+    }
+
+}
