c58558c069f8adb3ba064b95fcd87f01156d1e8c
Youssef Raafat <yraafat@instabug.com>
2022-04-21 14:37:06 +0200

    Export JS Modules with `runs`

--- example/www/js/index.js
+++ example/www/js/index.js
@@ -1,13 +1,16 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+  const Instabug = cordova.require("instabug-cordova.Instabug");
+  const BugReporting = cordova.require("instabug-cordova.BugReporting");
+
   console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
   document.getElementById("deviceready").classList.add("ready");
 
   // Initialize Instabug SDK
-  cordova.plugins.instabug.start(
+  Instabug.start(
     "cb518c145decef204b0735cb7efa6016",
-    [cordova.plugins.bugReporting.invocationEvents.button],
+    [BugReporting.invocationEvents.button],
     () => console.log("Instabug initialized."),
     (error) => console.log("Instabug could not be initialized - " + error)
   );
